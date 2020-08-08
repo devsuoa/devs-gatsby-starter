fcf02f796c0b0cc22e35372967c5cf5e6d6258e3
Alan.Thomas <alanunny@outlook.com>
2020-08-08 13:56:53 +1200

    changed to devs-starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,19 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  
+  plugins: [
+    
+    {
+      resolve: `gatsby-source-filesystem`, // this plugin loads files from the filesystem
+      options: {
+        name: `blog`, 
+        path: `${__dirname}/blog` // load in files from /blog
+      }
+    },
+
+    `gatsby-transformer-remark`, // this plugin parses markdown files that we loaded in
+
+    `gatsby-plugin-netlify-cms` // this plugin will create a cms at /admin path
+  ],
 }
